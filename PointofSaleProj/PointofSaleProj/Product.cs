2cfd8f75b4018da30c909273afcd0b2352efcdea
Tim Holleque <timothy.t.holleque@gmail.com>
2023-04-27 10:31:51 -0400

    added item class

---
+++ PointofSaleProj/PointofSaleProj/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointofSaleProj
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public Product(string Name, string Category, string Description, double Price)
+        {
+            this.Name = Name;  
+            this.Category = Category;
+            this.Description = Description;
+            this.Price = Price;
+        } 
+    }
+}
